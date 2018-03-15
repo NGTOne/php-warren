be8f581a11f520576f7aa2f2828323792c626140
Sebastian Lenartowicz <sebastian@azavista.com>
2018-03-15 07:52:31 +0100

    Added collection of actions

--- src/RabbitConsumer.php
+++ src/RabbitConsumer.php
@@ -6,13 +6,32 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 use Warren\ConnectionInterface;
+use Warren\AsynchronousAction;
+use Warren\SynchronousAction;
 
 class RabbitConsumer
 {
     private $conn;
+    private $asyncActions = [];
+    private $syncActions = [];
 
     public function __construct(ConnectionInterface $conn)
     {
         $this->conn = $conn;
+        $this->conn->connect();
+    }
+
+    public function addAsynchronousAction(
+        AsynchronousAction $action,
+        string $name
+    ) {
+        $this->asyncActions[$name] = $action;
+    }
+
+    public function addSynchronousAction(
+        SynchronousAction $action,
+        string $name
+    ) {
+        $this->syncActions[$name] = $action;
     }
 }
