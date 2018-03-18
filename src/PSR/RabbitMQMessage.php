17936e6814cd208db7ca8a10ee98dae9b60f40c0
Sebastian Lenartowicz <sebastian@azavista.com>
2018-03-18 14:13:30 +0100

    Re-implemented using Guzzle\Request

--- src/PSR/RabbitMQMessage.php
+++ src/PSR/RabbitMQMessage.php
@@ -4,11 +4,12 @@ namespace Warren\PSR;
 
 use Psr\Http\Message\MessageInterface;
 use GuzzleHttp\Psr7\MessageTrait;
+use GuzzleHttp\Psr7\Request;
 
 // RabbitMQ messages aren't _quite_ like HTTP messages, but there's
 // enough parallels that we can shoehorn one into a PSR7 MessageInterface
 // object just fine
-abstract class RabbitMQMessage implements MessageInterface
+abstract class RabbitMQMessage extends Request implements MessageInterface
 {
     private $version = "0-9-1";
 
--- src/PSR/RabbitMQRequest.php
+++ src/PSR/RabbitMQRequest.php
@@ -6,35 +6,11 @@ use Psr\Http\Message\RequestInterface;
 
 class RabbitMQRequest extends RabbitMQMessage implements RequestInterface
 {
-    public function getRequestTarget()
-    {
-
-    }
-
-    public function withRequestTarget($requestTarget)
-    {
-
-    }
-
-    public function getMethod()
-    {
-
-    }
-
-    public function withMethod($method)
-    {
-
-    }
-
-    public function getUri()
-    {
-
-    }
-
-    public function withUri(
-        \Psr\Http\Message\UriInterface $uri,
-        $preserveHost = false
+    public function __construct(
+        $headers = [],
+        $body = null,
+        $version = '0-9-1'
     ) {
-
+        parent::__construct("RabbitMQ", "/", $headers, $body, $version);
     }
 }
