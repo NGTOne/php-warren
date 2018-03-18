177a20a8ff5a6086e74db0fb83497b97237a5a02
Sebastian Lenartowicz <sebastian@azavista.com>
2018-03-18 10:01:33 +0100

    Added an abstract base class

---
+++ src/PSR/RabbitMQMessage.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Warren\PSR;
+
+use Psr\Http\Message\MessageInterface;
+
+// RabbitMQ messages aren't _quite_ like HTTP messages, but there's
+// enough parallels that we can shoehorn one into a PSR7 MessageInterface
+// object just fine
+abstract class RabbitMQMessage implements MessageInterface
+{
+    private $version = "0-9-1";
+    private $headers = [];
+    private $headerNames = [];
+
+    public function getProtocolVersion()
+    {
+        return $this->version;
+    }
+
+    public function withProtocolVersion($version)
+    {
+        $req = clone $this;
+        $req->version = $version;
+        return $req;
+    }
+
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    private function headerVal($name)
+    {
+        return $this->headers[$this->headerNames[strtolower($name)]];
+    }
+
+    public function getHeader($name)
+    {
+        return $this->hasHeader($name) ? $this->headerVal($name) : [];
+    }
+
+    public function getHeaderLine($name)
+    {
+        if (!$this->hasHeader($name)) {
+            return "";
+        }
+
+        $header = $this->headerVal($name);
+        return is_array($header) ? implode($header, ", ") : $header;
+    }
+
+    public function hasHeader($name)
+    {
+        return isset($this->headerNames[strtolower($name)]);
+    }
+
+    public function withHeader($name, $value)
+    {
+        $req = clone $this;
+        $req->headerNames[strtolower($name)] = $name;
+        $req->headers[$name] = $value;
+        return $req;
+    }
+
+    public function withAddedHeader($name, $value)
+    {
+
+    }
+
+    public function withoutHeader($name)
+    {
+
+    }
+
+    public function getBody()
+    {
+
+    }
+
+    public function withBody(\Psr\Http\Message\StreamInterface $body)
+    {
+
+    }
+}
--- src/PSR/RabbitMQRequest.php
+++ src/PSR/RabbitMQRequest.php
@@ -4,85 +4,8 @@ namespace Warren\PSR;
 
 use Psr\Http\Message\RequestInterface;
 
-// RabbitMQ messages aren't _quite_ like HTTP messages, but there's
-// enough parallels that we can shoehorn one into a PSR7 RequestInterface
-// object just fine
-class RabbitMQRequest implements RequestInterface
+class RabbitMQRequest extends RabbitMQMessage implements RequestInterface
 {
-    private $version = "0-9-1";
-    private $headers = [];
-    private $headerNames = [];
-
-    public function getProtocolVersion()
-    {
-        return $this->version;
-    }
-
-    public function withProtocolVersion($version)
-    {
-        $req = clone $this;
-        $req->version = $version;
-        return $req;
-    }
-
-    public function getHeaders()
-    {
-        return $this->headers;
-    }
-
-    private function headerVal($name)
-    {
-        return $this->headers[$this->headerNames[strtolower($name)]];
-    }
-
-    public function getHeader($name)
-    {
-        return $this->hasHeader($name) ? $this->headerVal($name) : [];
-    }
-
-    public function getHeaderLine($name)
-    {
-        if (!$this->hasHeader($name)) {
-            return "";
-        }
-
-        $header = $this->headerVal($name);
-        return is_array($header) ? implode($header, ", ") : $header;
-    }
-
-    public function hasHeader($name)
-    {
-        return isset($this->headerNames[strtolower($name)]);
-    }
-
-    public function withHeader($name, $value)
-    {
-        $req = clone $this;
-        $req->headerNames[strtolower($name)] = $name;
-        $req->headers[$name] = $value;
-        return $req;
-    }
-
-    public function withAddedHeader($name, $value)
-    {
-
-    }
-
-    public function withoutHeader($name)
-    {
-
-    }
-
-    public function getBody()
-    {
-
-    }
-
-    public function withBody(\Psr\Http\Message\StreamInterface $body)
-    {
-
-    }
-
     public function getRequestTarget()
     {
 
