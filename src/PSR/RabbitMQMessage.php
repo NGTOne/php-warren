f29f4726e4b20c831352d68a6b728e37450100cf
Sebastian Lenartowicz <sebastian@azavista.com>
2018-03-18 10:33:11 +0100

    Reimplemented withHeader using withoutHeader

--- src/PSR/RabbitMQMessage.php
+++ src/PSR/RabbitMQMessage.php
@@ -57,10 +57,7 @@ abstract class RabbitMQMessage implements MessageInterface
 
     public function withHeader($name, $value)
     {
-        $req = clone $this;
-        if ($req->hasHeader($name)) {
-            unset($req->headers[$req->headerNames[strtolower($name)]]);
-        }
+        $req = $this->withoutHeader($name);
         $req->headerNames[strtolower($name)] = $name;
         $req->headers[$name] = $value;
         return $req;
