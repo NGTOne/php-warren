67168ec3b5959c2f378377d9b5f73bd34c19de76
Sebastian Lenartowicz <sebastian@azavista.com>
2018-04-07 17:58:36 +0200

    Handle SIGINT by default as well

--- src/RabbitConsumer.php
+++ src/RabbitConsumer.php
@@ -43,7 +43,8 @@ class RabbitConsumer
         $this->replyErrorHandler = new EchoingErrorHandler;
         $this->signalHandler = new ExitingSignalHandler([
             'SIGTERM',
-            'SIGHUP'
+            'SIGHUP',
+            'SIGINT'
         ]);
     }
 
